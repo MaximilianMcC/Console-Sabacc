dece8dff52b6ec3f96bf35903b666c16f4f9851d
Max <max@maximilian.co.nz>
2023-01-04 01:44:15 +1300

    Add title thing

--- Program.cs
+++ Program.cs
@@ -5,6 +5,17 @@
     {
         Console.Title = "Console Sabacc";
 
+        // Main title screen thing
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(@"     _______.     ___      .______        ___       ______   ______ ");
+        Console.WriteLine(@"    /       |    /   \     |   _  \      /   \     /      | /      |");
+        Console.WriteLine(@"   |   (----`   /  ^  \    |  |_)  |    /  ^  \   |  ,----'|  ,----'");
+        Console.WriteLine(@"    \   \      /  /_\  \   |   _  <    /  /_\  \  |  |     |  |     ");
+        Console.WriteLine(@".----)   |    /  _____  \  |  |_)  |  /  _____  \ |  `----.|  `----.");
+        Console.WriteLine(@"|_______/    /__/     \__\ |______/  /__/     \__\ \______| \______|");
+        Console.ResetColor();
+
+
 
         // Begin a new game
         Game game = new Game();
