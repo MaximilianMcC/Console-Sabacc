02ebdef7feec8ced8274714350b40844bc9fbc7b
Max <max@maximilian.co.nz>
2023-01-04 01:49:41 +1300

    Game is now played in game class

--- Program.cs
+++ Program.cs
@@ -5,33 +5,31 @@
     {
         Console.Title = "Console Sabacc";
 
-        // Main title screen thing
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine(@"     _______.     ___      .______        ___       ______   ______ ");
-        Console.WriteLine(@"    /       |    /   \     |   _  \      /   \     /      | /      |");
-        Console.WriteLine(@"   |   (----`   /  ^  \    |  |_)  |    /  ^  \   |  ,----'|  ,----'");
-        Console.WriteLine(@"    \   \      /  /_\  \   |   _  <    /  /_\  \  |  |     |  |     ");
-        Console.WriteLine(@".----)   |    /  _____  \  |  |_)  |  /  _____  \ |  `----.|  `----.");
-        Console.WriteLine(@"|_______/    /__/     \__\ |______/  /__/     \__\ \______| \______|");
-        Console.ResetColor();
+        while (true)
+        {
+            Console.Clear();
+
+            // Main title screen thing
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"     _______.     ___      .______        ___       ______   ______ ");
+            Console.WriteLine(@"    /       |    /   \     |   _  \      /   \     /      | /      |");
+            Console.WriteLine(@"   |   (----`   /  ^  \    |  |_)  |    /  ^  \   |  ,----'|  ,----'");
+            Console.WriteLine(@"    \   \      /  /_\  \   |   _  <    /  /_\  \  |  |     |  |     ");
+            Console.WriteLine(@".----)   |    /  _____  \  |  |_)  |  /  _____  \ |  `----.|  `----.");
+            Console.WriteLine(@"|_______/    /__/     \__\ |______/  /__/     \__\ \______| \______|");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(@"                                                                    ");
+            Console.WriteLine(@"                                                                    ");
+            Console.WriteLine(@"                       PRESS ANY KEY TO START                       ");
+            Console.ResetColor();
 
 
 
-        // Begin a new game
-        Game game = new Game();
-        game.Start();
+            // Begin a new game
+            Game game = new Game();
+            game.Start();
+        }
 
-        // Play the game
-        game.Round();
-        game.Round();
-        game.Round();
-
-        // Check for who the winner is 
-        game.End();
-
-
-        Console.WriteLine("\n\nEnd of program...");
-        Console.ReadKey();
     }
 
 }
