d27c06e93c17d62a06f992e446a179a93a61224e
Max <max@maximilian.co.nz>
2023-01-27 15:44:37 +1300

    Completely restarting as I forgot what I was doing

--- Program.cs
+++ Program.cs
@@ -1,35 +1,10 @@
-﻿class Program
+class Program
 {
     
     public static void Main(string[] args)
     {
-        Console.Title = "Console Sabacc";
-
-        while (true)
-        {
-            Console.Clear();
-
-            // Main title screen thing
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(@"     _______.     ___      .______        ___       ______   ______ ");
-            Console.WriteLine(@"    /       |    /   \     |   _  \      /   \     /      | /      |");
-            Console.WriteLine(@"   |   (----`   /  ^  \    |  |_)  |    /  ^  \   |  ,----'|  ,----'");
-            Console.WriteLine(@"    \   \      /  /_\  \   |   _  <    /  /_\  \  |  |     |  |     ");
-            Console.WriteLine(@".----)   |    /  _____  \  |  |_)  |  /  _____  \ |  `----.|  `----.");
-            Console.WriteLine(@"|_______/    /__/     \__\ |______/  /__/     \__\ \______| \______|");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine(@"                                                                    ");
-            Console.WriteLine(@"                                                                    ");
-            Console.WriteLine(@"                       PRESS ANY KEY TO START                       ");
-            Console.ResetColor();
-
-
-
-            // Begin a new game
-            Game game = new Game();
-            game.Start();
-        }
-
+        Console.WriteLine("Sabacc\n\n\n");
+        Console.ReadKey();
     }
 
 }
