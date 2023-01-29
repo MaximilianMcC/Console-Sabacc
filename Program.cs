1dbb15288d1c1815966fbac44cd05f3c82635d68
Max <max@maximilian.co.nz>
2023-01-29 19:38:04 +1300

    Added main menu

--- Program.cs
+++ Program.cs
@@ -1,15 +1,80 @@
 class Program
 {
     
-    public static void Main(string[] args)
+    static bool animationPlaying = true;
+
+    static void Main(string[] args)
     {
-        Console.WriteLine("Sabacc\n\n\n");
+        // Make a star background
+        Random random = new Random();
+        for (int y = 0; y < Console.WindowHeight; y++)
+        {
+            for (int x = 0; x < Console.WindowWidth; x++)
+            {
+                if (random.Next(1, 15) == 1) Console.Write('*');
+                else Console.Write(' ');
+            }
+
+            Console.Write("\n");
+        }
+
+        // Main Sabacc menu
+        Console.SetCursorPosition(0, 5);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("\n\n\n\n");
+        CentreText(@"     _______.     ___      .______        ___       ______   ______ ");
+        CentreText(@"    /       |    /   \     |   _  \      /   \     /      | /      |");
+        CentreText(@"   |   (----`   /  ^  \    |  |_)  |    /  ^  \   |  ,----'|  ,----'");
+        CentreText(@"    \   \      /  /_\  \   |   _  <    /  /_\  \  |  |     |  |     ");
+        CentreText(@".----)   |    /  _____  \  |  |_)  |  /  _____  \ |  `----.|  `----.");
+        CentreText(@"|_______/    /__/     \__\ |______/  /__/     \__\ \______| \______|");
+        Console.ResetColor();
+
+        // Start the flashing text animation
+        new Thread(() => FlashingTextAnimation("--- PRESS ANY KEY TO START ---", 20)).Start();
+
+        // Get console input
+        while (true)
+        {
+            if (Console.KeyAvailable)
+            {
+                Console.ReadKey();
+                animationPlaying = false;
+                Console.Clear();
+                
+                // Start the game
+                Game game = new Game();
+                game.NewGame();
+            }
+        }
+    }
 
 
-        Game game = new Game();
-        game.NewGame();
+    // Flashing text animation
+    static void FlashingTextAnimation(string text, int cursorTop)
+    {
+        bool showText = true;
+        while (animationPlaying == true)
+        {
+            // Change the colors depending on the state
+            if (showText) Console.ForegroundColor = ConsoleColor.White;
+            else Console.ForegroundColor = ConsoleColor.DarkGray;
 
-        Console.WriteLine("Press any key to continue...");
-        Console.ReadKey();
+            // Write the text
+            Console.SetCursorPosition(Console.CursorLeft, cursorTop);
+            CentreText(text);
+            Console.ResetColor();
+
+            // Change the state of the text, then wait
+            showText = !showText;
+            Thread.Sleep(500); //? 500ms is 0.5 seconds            
+        }
+    }
+
+    // Place text in the centre of the console
+    static void CentreText(string text)
+    {
+        Console.SetCursorPosition((Console.WindowWidth - text.Length) / 2, Console.CursorTop);
+        Console.WriteLine(text);
     }
 }
